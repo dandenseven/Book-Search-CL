c791135c64ef8d0ac8bf1ac4a98d554cd7c0f16f
dandenseven <31233404+dandenseven@users.noreply.github.com>
2021-12-22 00:29:16 -0500

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,30 @@
 #!/usr/bin/env node
+require('dotenv').config();
+
+
+const apiKey = process.env.API_KEY
+console.log(apiKey)
 
 const yargs = require("yargs");
 const axios = require("axios")
 
 const options = yargs
-    .usage("Usage: -n <name>")
-    .option("n", {alias: "name", describe: "Authors Name", type: "string", demandOption: true }
-    )
+    .usage("Usage: -n ")
+    .option("n", {alias: "name", describe: "Authors Name", type: "string", demandOption: true })
+    .option("s", {alais: "book", describe: "Title", type: "string"})
+    .option("p", {alais: "publisher", describe: "Publishers Name", type: "string"})
     .argv;
-const greeting = `Hello, ${options.name}!`;
-console.log(greeting);
 
-console.log("Here's a random joke for you:");
+// calling google book api
+const url = options.search ? `GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=API_KEY{(options.search)}`;
 
-const url = "https://in"
+axios.get(url, {headers: {Accept: "application/json"} })
+    .then(res => {
+        if (options.search) {
+            var _ = require("underscore");
+            var bookList = []
+            res.data.results.forEach(element => {
+                
+            });
+        }
+    })
