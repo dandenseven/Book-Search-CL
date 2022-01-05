2694ddf72cd9cedab23ef111200df2eb802a9536
dandenseven <31233404+dandenseven@users.noreply.github.com>
2022-01-05 03:50:44 -0500

    Refactored code
    Changes to show use of Google Api & making request to search for user input using search terms.

---
+++ bin/test_index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+require('dotenv').config();
+
+
+const apiKey = process.env.API_KEY
+console.log(apiKey)
+
+const yargs = require("yargs");
+const axios = require("axios")
+
+const options = yargs
+    .usage("Usage: -n ")
+    .option("n", {alias: "name", describe: "Authors Name", type: "string", demandOption: true })
+    .option("s", {alais: "book", describe: "Title", type: "string"})
+    .option("p", {alais: "publisher", describe: "Publishers Name", type: "string"})
+    .argv;
+
+// calling google book api
+const url = options.search ? `https://www.googleapis.com/books/v1/volumes?q=search+terms{escape(options.search)}`
+
+axios.get(url, {headers: {Accept: "application/json"} })
+    .then(res => {
+        if (options.search) {
+            var _ = require("underscore");
+            var bookList = []
+            res.data.results.forEach(element => {
+                
+            });
+        }
+});
